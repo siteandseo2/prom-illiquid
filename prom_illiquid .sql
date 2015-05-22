6bb58659e73c4f4c84e0330a0a8544b6a20c7cf9
Alexander Prytula <isitinme88@gmail.com>
2015-05-22 15:35:34 +0300

    fix cache bugs

--- prom_illiquid .sql
+++ prom_illiquid .sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Май 20 2015 г., 16:10
+-- Время создания: Май 22 2015 г., 14:46
 -- Версия сервера: 5.5.43-0ubuntu0.14.04.1
 -- Версия PHP: 5.5.9-1ubuntu4.9
 
@@ -31,25 +31,10 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `name` varchar(250) CHARACTER SET utf8 NOT NULL,
   `status` varchar(10) CHARACTER SET utf8 NOT NULL,
   `link` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `fp_id` int(11) NOT NULL,
+  `image_path` varchar(200) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=13 ;
-
---
--- Дамп данных таблицы `categories`
---
-
-INSERT INTO `categories` (`id`, `name`, `status`, `link`) VALUES
-(1, 'Отопление', 'enable', 'otoplenie'),
-(2, 'Строительство', 'enable', 'stroitelstvo'),
-(3, 'Промышленное оборудование', 'enable', 'prom_machine'),
-(4, 'Оборудование для предоставления услуг', 'enable', 'service_machine'),
-(5, 'Электрооборудование', 'enable', 'electromachine'),
-(6, 'Инструмент', 'enable', 'tools'),
-(7, 'Сельхозпродукция и техника', 'enable', 'agricultural_product'),
-(8, 'Металл, пластик, резина', 'enable', 'metal_plastic_rubber'),
-(9, 'Безопасность и защита', 'enable', 'security&Protection'),
-(10, 'Контрольно-измерительные приборы', 'enable', 'test&Measurement'),
-(11, 'Сырье и материалы', 'enable', 'raw_materials');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -71,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 --
 
 INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('b53b6c0ddb1ce1503d077ef850a1d965', '127.0.0.1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36', 1432127205, 'a:2:{s:9:"user_data";s:0:"";s:5:"admin";a:3:{s:4:"name";s:5:"Admin";s:5:"email";s:5:"admin";s:9:"user_type";s:5:"admin";}}');
+('1f76fbe71ba7bce5b3fa9c1d25402efd', '127.0.0.1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36', 1432295122, 'a:1:{s:5:"admin";a:3:{s:4:"name";s:5:"Admin";s:5:"email";s:5:"admin";s:9:"user_type";s:5:"admin";}}');
 
 -- --------------------------------------------------------
 
@@ -93,8 +78,40 @@ CREATE TABLE IF NOT EXISTS `focus_products` (
 INSERT INTO `focus_products` (`id`, `name`, `status`) VALUES
 (1, 'Промышленные товары', 'enable'),
 (2, 'Потребительские товары', 'enable'),
-(3, 'Услуги', 'enable'),
-(5, 'Что то еще', 'enable');
+(3, 'Услуги', 'enable');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `image_path` varchar(200) NOT NULL,
+  `link` varchar(200) NOT NULL,
+  `price` int(11) NOT NULL,
+  `subcat_id` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `subcategories`
+--
+
+CREATE TABLE IF NOT EXISTS `subcategories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `image_path` varchar(200) NOT NULL,
+  `link` varchar(200) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
