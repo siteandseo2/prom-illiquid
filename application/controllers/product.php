16cc5d286f7f3d606269fda421254e83f8aaa3f9
mprihodko <prihodkomaks92@gmail.com>
2015-05-22 14:49:17 +0300

    new db

---
+++ application/controllers/product.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of product
+ *
+ * @author baccardi
+ */
+class Product extends CI_Controller {
+
+    public $data = '';
+    public $data_db = '';
+            public$cat_id = '';
+
+    function __construct() {
+        parent::__construct();
+        /* load header */
+        if (!empty($this->session->userdata('user'))) {
+            $this->data['user'] = @$this->session->userdata('user');
+            $this->load->view("templates/header_user", $this->data);
+        } else {
+            $this->load->view("templates/header");
+        }
+        /* load model product */
+        $this->load->model('product_m');
+        $this->data['products'] = $this->product_m->get_all_product();
+    }
+
+    function get_all_product() {
+        $this->load->view("pages/products", $this->data);
+        $this->load->view("templates/footer");
+    }
+
+    function get_product($id) {
+        $this->data_db['product'] = $this->product_m->get_product($id);
+        $this->data_db['cat_name'] = $this->product_m->get_product_cat($this->data_db['product']['0']['cat_id']);
+        $this->load->view("pages/item", $this->data_db);
+        $this->load->view("templates/footer");
+    }
+
+}
---
+++ application/models/product_m.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of product_m
+ *
+ * @author baccardi
+ */
+class Product_m extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function get_all_product() {
+        $query = $this->db->get('product');
+        return $query->result_array();
+    }
+
+    function get_product($id) {
+        $query = $this->db->where('id', $id)->get('product');
+        return $query->result_array();
+    }
+   
+     function get_product_cat($cat_id) {
+        $query = $this->db->where('id', $cat_id)->get('categories');
+        return $query->result_array();
+    }
+}
