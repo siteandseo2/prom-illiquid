a614cbc0352a337c240b2c6bd03c3c197e82c99e
mprihodko <prihodkomaks92@gmail.com>
2015-05-25 17:54:30 +0300

    clean console

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,3 +1,7 @@
+<?php
+$a = $_SERVER['REQUEST_URI'];
+echo $a;
+?>
 <!-- jQuery -->
 <script src="<?= base_url(); ?>../../../js/jquery.js"></script>
 
@@ -6,18 +10,20 @@
 
 <!-- Morris Charts JavaScript -->
 <script src="<?= base_url(); ?>../../../js/plugins/morris/raphael.min.js"></script>
-<script src="<?= base_url(); ?>../../../js/plugins/morris/morris.min.js"></script>
-<script src="<?= base_url(); ?>../../../js/plugins/morris/morris-data.js"></script>
-
-<!-- Flot Charts JavaScript -->
-<!--[if lte IE 8]><script src="js/excanvas.min.js"></script><![endif]-->
-<script src="<?= base_url(); ?>../../../js/plugins/flot/jquery.flot.js"></script>
-<script src="<?= base_url(); ?>../../../js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-<script src="<?= base_url(); ?>../../../js/plugins/flot/jquery.flot.resize.js"></script>
-<script src="<?= base_url(); ?>../../../js/plugins/flot/jquery.flot.pie.js"></script>
-<script src="<?= base_url(); ?>../../../js/plugins/flot/flot-data.js"></script>
+<?php if ($a == '/admin/charts') { ?>
+    <script src="<?= base_url(); ?>../../../js/plugins/morris/morris.min.js"></script>
+    <script src="<?= base_url(); ?>../../../js/plugins/morris/morris-data.js"></script>
 
 
+    <!-- Flot Charts JavaScript -->
+    <!--[if lte IE 8]><script src="js/excanvas.min.js"></script><![endif]-->
+    <script src="<?= base_url(); ?>../../../js/plugins/flot/jquery.flot.js"></script>
+    <script src="<?= base_url(); ?>../../../js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+    <script src="<?= base_url(); ?>../../../js/plugins/flot/jquery.flot.resize.js"></script>
+    <script src="<?= base_url(); ?>../../../js/plugins/flot/jquery.flot.pie.js"></script>
+    <script src="<?= base_url(); ?>../../../js/plugins/flot/flot-data.js"></script>
+
+<?php } ?>
 
 <!-- Main Back Js -->
 <!--<script src="<?= base_url(); ?>../../../js/back_end.js"></script>-->
