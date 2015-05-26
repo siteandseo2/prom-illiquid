e219804a65cade135552b98f4838a33550e1232e
mprihodko <prihodkomaks92@gmail.com>
2015-05-26 17:21:25 +0300

    json link

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -1,16 +1,23 @@
 <?php
 
 class Ajax extends CI_Controller {
+
     public $data;
-    
+
     function __construct() {
         parent::__construct();
         $this->load->model('category_m');
     }
 
-    public function change_tabs($tab = '1') {        
-        $categories=  $this->category_m->get_category_list($tab);
-        echo json_encode($categories);         
+    public function change_tabs($tab = '1') {
+        $categories = $this->category_m->get_category_list($tab);
+        $json=array();
+        foreach ($categories as $num => $column){             
+            foreach($column as $name =>$value){
+                $json[$name][$num]=$value;
+            }
+        }
+        echo json_encode($json);
     }
 
 }
