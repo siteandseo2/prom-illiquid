bc605e6b4689959f2e03cb9d24d9a54ad7547bb5
mprihodko <prihodkomaks92@gmail.com>
2015-05-26 11:48:22 +0300

    show

---
+++ application/controllers/subcat_show.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of subcat_show
+ *
+ * @author baccardi
+ */
+class subcat_show {
+    //put your code here
+}
