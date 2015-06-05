7a69bf37baa988d1e90fa4739f90756d2088f93c
mprihodko <prihodkomaks92@gmail.com>
2015-06-05 17:08:52 +0300

    edit menu

--- application/controllers/menu.php
+++ application/controllers/menu.php
@@ -13,8 +13,40 @@
  */
 class Menu extends CI_Controller {
 
+    public $data;
+    public $data_menu;
+
     function __construct() {
         parent::__construct();
+        if (!empty($this->session->userdata('admin'))) {
+            $this->data['admin'] = $this->session->userdata('admin');
+            $this->load->view("admin/header", $this->data);
+        }
+
+
+        /* load menu */
+        $this->load->model('main_m');
+        $this->data['menu'] = $this->main_m->get_menu();
+        $this->data['fst_level'] = $this->main_m->get_fst_l();
+        $this->data['scnd_level'] = $this->main_m->get_snd_l();
+        $this->data['trd_level'] = $this->main_m->get_trd_l();
+        /* load categories */
+        $this->load->model('category_m');
+        $this->data['cat_list'] = $this->category_m->category_list();
+
+        /* load users */
+
+        $this->load->model('user_model');
+        $this->data['user'] = $this->user_model->get_user();
+
+        /* load focus product */
+
+        $this->data['fpl'] = $this->category_m->focus_product_list();
+
+        /* load subcategories list */
+        $this->load->model('subcategories_m');
+
+        $this->data['list'] = $this->subcategories_m->get_subcategories_list();
     }
 
     public function edit_menu() {
@@ -27,26 +59,26 @@ class Menu extends CI_Controller {
                 if ($id == $key)
                     $name = $value;
             }
-            echo $name;
+//            echo $name;
 
             foreach ($this->input->post('link') as $key => $value) {
                 if ($id == $key)
                     $link = $value;
             }
-            echo $link;
+//            echo $link;
 
 
             foreach ($this->input->post('parent') as $key => $value) {
                 if ($id == $key)
                     $p_id = $value;
             }
-            echo $p_id;
+//            echo $p_id;
 
             foreach ($this->input->post('parent2') as $key => $value) {
                 if ($id == $key)
                     $p_id2 = $value;
             }
-            echo $p_id2;
+//            echo $p_id2;
 
             $this->db->query("UPDATE menu SET name='$name' WHERE id='$id'");
             $this->db->query("UPDATE menu SET link='$link'  WHERE id='$id'");
@@ -151,10 +183,69 @@ class Menu extends CI_Controller {
     /* END  function delete_menu  */
 
     public function get_menu_list() {
+        $this->load->view('admin/main', $this->data);
         $this->delete_menu();
         $this->change_type();
         $this->edit_menu();
-        redirect(base_url('admin/main'));
+        $this->load->view('admin/footer');
+    }
+
+    function add_menu() {
+        if (isset($_POST['add_item_menu'])) {
+            $this->data_menu['owner'] = $this->input->post('owner');
+            if ($this->input->post('group') == 'r') {
+                $this->data_menu['type'] = 'd';
+                $this->data_menu['p_id'] = $this->input->post('parent');
+                $this->data_menu['p_id2'] = '0';
+            }
+            if ($this->input->post('group') == 'd') {
+                $this->data_menu['type'] = 'dd';
+                $this->data_menu['p_id'] = '0';
+                $this->data_menu['p_id2'] = $this->input->post('parent');
+            }
+            if ($this->input->post('group') == 'default') {
+                $this->data_menu['type'] = 'r';
+                $this->data_menu['p_id'] = '0';
+                $this->data_menu['p_id2'] = '0';
+            }
+            $this->data_menu['name'] = $this->input->post('name');
+            $this->data_menu['status'] = $this->input->post('status');
+            if (!empty($this->input->post('link'))) {
+                $this->data_menu['link'] = $this->input->post('link');
+                if ($this->input->post('owner') == 'user') {
+                    if (!file_exists(APPPATH . '/views/userpages/' . $this->input->post('link') . '.php')) {
+                        $fp = fopen('application/views/userpages/' . $this->input->post('link') . '.php', 'a');
+//                $mytext = "Это строку необходимо нам записать\r\n"; // Исходная строка
+//                $test = fwrite($fp, $mytext); // Запись в файл
+                        fclose($fp);
+                        $this->main_m->insert_item($this->data_menu);
+                        redirect(base_url('admin/main'));
+                    } else {
+                        $this->data['error'] = 'Cтраница с таким именем уже существует!';
+                        $this->load->view('admin/error', $this->data);
+                        $this->load->view('admin/footer');
+                    }
+                }
+                if ($this->input->post('owner') == 'admin') {
+                    if (!file_exists(APPPATH . '/views/pages/' . $this->input->post('link') . '.php')) {
+                        $fp = fopen('application/views/pages/' . $this->input->post('link') . '.php', 'a');
+//                $mytext = "Это строку необходимо нам записать\r\n"; // Исходная строка
+//                $test = fwrite($fp, $mytext); // Запись в файл
+                        fclose($fp);
+                        $this->main_m->insert_item($this->data_menu);
+                        redirect(base_url('admin/main'));
+                    } else {
+                        $this->data['error'] = 'Cтраница с таким именем уже существует!';
+                        $this->load->view('admin/error', $this->data);
+                        $this->load->view('admin/footer');
+                    }
+                }
+            } else {
+                $this->data_menu['link'] = '#';
+                $this->main_m->insert_item($this->data_menu);
+                redirect(base_url('admin/main'));
+            }
+        }
     }
 
 }
