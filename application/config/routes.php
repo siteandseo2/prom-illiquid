5ce4506124087a12f6d9da4f8191130979602e74
mprihodko <prihodkomaks92@gmail.com>
2015-06-09 10:35:04 +0300

    company info edit

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,6 +56,7 @@ $route['ajax/filter_by_categories']='ajax/filter_by_categories/$1';
 $route['ajax/change_location']='ajax/change_location';
 $route['admin/ajax/add_menu_item']='ajax/change_item_menu';
 $route['admin/ajax/filter_by_categories']='ajax/filter_by_categories/$1';
+$route['company_info/ajax/change_location']='ajax/change_location';
 $route['account/ajax/change_location']='ajax/change_location';
 $route['admin/ajax/filter_by_group']='ajax/filter_by_group/$1';
 $route['admin/products/filter_by_category'] = 'ajax/filter_by_category';
