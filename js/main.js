8acdaf2f201f09325a47f30684fb898bba359b46
Alexander Prytula <isitinme88@gmail.com>
2015-06-12 13:24:10 +0300

    switcher!

--- js/main.js
+++ js/main.js
@@ -211,5 +211,33 @@
 		}, 10);
 	});
 	
+	/* Cabinet Switch */
+	
+	(function() {
+		
+		$('.entrance .query').click(function() {
+			$('.changeRole').slideToggle();
+		});
+		
+		try {
+			
+			$('[name="role"]').bootstrapSwitch().on('switchChange.bootstrapSwitch', function(event, state) {
+			  
+			  $.ajax({
+				  type: 'GET',
+				  url: 'ajax/change_role',
+				  data: {
+					  id: +state
+				  }
+			  });
+			  
+			});
+					
+		} catch( e ) {
+			console.log( e.type + ' : ' + e.message );
+		}
+		
+	}());
+	
 	
 });
